1ac5907a180135ab199bf1b8069dc6c0f316d803
yantao <yan.tao.work@gmail.com>
2021-06-10 11:47:03 +0200

    refactor pebble ui

--- src/pages/Pebble/SectionContainer.tsx
+++ src/pages/Pebble/SectionContainer.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import {Box} from "@chakra-ui/react";
 
-export const SectionContainer=({children})=>{
+export const SectionContainer=({children,...props})=>{
 	return(
 		<Box
+			{...props}
 			maxWidth={{base: '90%', md: '80%', lg: '90%', '2xl': '1554px'}}
 			mx={'auto'}
 		>
