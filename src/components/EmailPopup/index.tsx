de890886b22f4d221df5689cdf678f8c92ed73fc
jianly <m13221001019@163.com>
2021-07-14 14:49:01 +0800

    fix emial popup show

--- src/components/EmailPopup/index.tsx
+++ src/components/EmailPopup/index.tsx
@@ -22,10 +22,9 @@ export const EmailPopup = observer(({}) => {
   };
 
   useEffect(() => {
-    setIsOpen(true);
     if (window.localStorage.getItem("isShowEmail") === null) {
       setTimeout(() => {
-      
+        setIsOpen(true);
       }, 2000);
     }
   }, []);
